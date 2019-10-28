f886377bc05ce89f6914f540235f379a53f9501b
jcon1126 <55714236+jcon1126@users.noreply.github.com>
2019-10-28 12:25:54 -0700

    Update sketch.js

--- jcon1126.github.io/jcon1126.giyhub.io/example/sketch.js
+++ jcon1126.github.io/jcon1126.giyhub.io/example/sketch.js
@@ -1,6 +1,9 @@
 
 
-functionsetup()
-{createCanvas(480,120);}functiondraw()
-{if(mouseIsPressed){fill(0);}
-else{fill(255);}ellipse(mouseX,mouseY,80,80);}
+function setup()
+{createCanvas(480,120);}
+function draw()
+{if (mouseIsPressed){fill(0);}
+else
+{fill(255);}
+ellipse(mouseX,mouseY,80,80);}
