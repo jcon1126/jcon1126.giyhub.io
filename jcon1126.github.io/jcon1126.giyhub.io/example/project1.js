e11417d7ddee28792a6dad3ea8a9572ede5a0354
jcon1126 <55714236+jcon1126@users.noreply.github.com>
2019-10-28 11:12:42 -0700

    changes

--- jcon1126.github.io/jcon1126.giyhub.io/example/project1.js
+++ jcon1126.github.io/jcon1126.giyhub.io/example/project1.js
@@ -1,5 +1,9 @@
 functionsetup()
-{createCanvas(480,120);}
+{createCanvas(480,120);
+}
 functiondraw()
 {if(mouseIsPressed){fill(0);}
-else{fill(255);}ellipse(mouseX,mouseY,80,80);}
+else{fill(255);
+}
+ellipse(mouseX,mouseY,80,80);
+}
